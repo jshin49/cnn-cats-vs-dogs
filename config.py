ded1cd6e84e02f2b751c15029b065ec7c36a472e
Jay Shin <jshin49@gmail.com>
2017-05-03 01:51:06 +0800

    with_gpu

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config(object):
     # Data Processing
     train_dir = './data/train'
     test_dir = './data/test'
-    image_size = 64     # resize image to image_size*image_size
+    image_size = 150    # resize image to image_size*image_size
     channels = 3        # Channel Size
     split_rate = 0.2 	# Validation Data portion
     valid_size = int(25000 * split_rate)
