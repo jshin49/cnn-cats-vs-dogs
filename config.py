366e5c5fa6c2c50bc5ec86bd0d1bbbf0dc35e015
Jay Shin <jshin49@gmail.com>
2017-05-02 22:19:12 +0800

    full_lr0.001_img64

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     instantiation.
     """
     # Hyperparameters
-    lr = 0.01  		    # Learning Rate
+    lr = 0.001  		# Learning Rate
     dropout = 0.5  		# Dropout Rate
     batch_size = 16		# SGD Batch Size
     epochs = 10			# Number of Training Epochs
@@ -15,7 +15,7 @@ class Config(object):
     # Data Processing
     train_dir = './data/train'
     test_dir = './data/test'
-    image_size = 150    # resize image to image_size*image_size
+    image_size = 64     # resize image to image_size*image_size
     channels = 3        # Channel Size
     split_rate = 0.2 	# Validation Data portion
     valid_size = int(25000 * split_rate)
