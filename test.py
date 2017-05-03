7e1db2655a2848114f30c0cb2eca92594162bcdf
Jay Shin <jshin49@gmail.com>
2017-05-03 13:30:10 +0800

    Resolved memory issue

--- test.py
+++ test.py
@@ -26,18 +26,27 @@ model.restore()
 
 # Generate data and batches for each epoch
 train_data, validation_data, test_data = load_data()
-# train_images, train_labels = map(list, zip(*train_data))
+train_images, train_labels = map(list, zip(*train_data))
 # test_images, test_labels = map(list, zip(*test_data))
+
+train_batches = generate_train_batches(train_data, config.batch_size)
+train_batch = get_next_batch(train_batches)
+train_batch_images, train_batch_labels = map(list, zip(*train_batch))
+train_batch_images = np.array(train_batch_images)
+train_batch_labels = np.array(train_batch_labels)
+train_batch_images = train_batch_images.reshape(-1, config.image_size,
+                                                config.image_size, config.channels)
+
+pred1, acc1 = model.eval_batch(train_batch_images, train_batch_labels)
 
 val_batches = generate_train_batches(validation_data, 100)
 val_batch = get_next_batch(val_batches)
-validation_images, validation_labels = map(list, zip(*val_batch))
+val_batch_images, val_batch_labels = map(list, zip(*val_batch))
+val_batch_images = np.array(validation_images)
+val_batch_labels = np.array(validation_labels)
+val_batch_images = val_batch_images.reshape(-1, config.image_size,
+                                            config.image_size, config.channels)
 
-validation_images = np.array(validation_images)
-validation_labels = np.array(validation_labels)
-validation_images = validation_images.reshape(-1, config.image_size,
-                                              config.image_size, config.channels)
+pred2, acc2 = model.eval_batch(val_batch_images, val_batch_labels)
 
-pred, acc = model.eval_batch(validation_images, validation_labels)
-
-print(acc)
+print('Training Acc: %d, Validation Acc: %f' % (acc1, acc2))
