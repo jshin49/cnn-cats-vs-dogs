7ed2e6a1dca7fd91f24b043742a5344497ab393e
Jay Shin <jshin49@gmail.com>
2017-05-04 12:41:08 +0800

    changed valid size

--- config.py
+++ config.py
@@ -20,8 +20,8 @@ class Config(object):
     image_dir = './arrays/'
     image_size = 64     # resize image to image_size*image_size
     channels = 3        # Channel Size
-    valid_size = 1000
-    train_size = 24000
+    valid_size = 500
+    train_size = 24500
 
     # Saver
     model_name = 'cnn-tflayers-model-l2{}-dropout{}-batch{}-lr{}-img{}'.format(
