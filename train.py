c65565a4578eff23225cb121bed534a285b21c60
Jay Shin <jshin49@gmail.com>
2017-05-04 10:39:20 +0800

    exp lr

--- train.py
+++ train.py
@@ -20,15 +20,22 @@ def train(train_data, total_batch_size, validation_data=None, val_batch_size=Non
         avg_loss = 0
         avg_acc = 0
         train_batches = generate_train_batches(train_data, config.batch_size)
+        train_batch = get_next_batch(train_batches)
+
+        train_batch_images, train_batch_labels = map(
+            list, zip(*train_batch))
+        train_batch_images = np.array(train_batch_images).reshape(-1, config.image_size,
+                                                                  config.image_size, config.channels)
+        train_batch_labels = np.array(train_batch_labels).reshape(-1, 1)
 
         for step in tqdm(range(total_batch_size)):
-            train_batch = get_next_batch(train_batches)
+            # train_batch = get_next_batch(train_batches)
 
-            train_batch_images, train_batch_labels = map(
-                list, zip(*train_batch))
-            train_batch_images = np.array(train_batch_images).reshape(-1, config.image_size,
-                                                                      config.image_size, config.channels)
-            train_batch_labels = np.array(train_batch_labels).reshape(-1, 1)
+            # train_batch_images, train_batch_labels = map(
+            #     list, zip(*train_batch))
+            # train_batch_images = np.array(train_batch_images).reshape(-1, config.image_size,
+            #                                                           config.image_size, config.channels)
+            # train_batch_labels = np.array(train_batch_labels).reshape(-1, 1)
 
             summary, loss, acc = model.train_eval_batch(
                 train_batch_images, train_batch_labels, False)
@@ -71,12 +78,15 @@ model = Model(config, sess, graph)
 train_data, validation_data, test_data = load_data()
 
 # Hyperparameter Tuning (Choose Best)
-image_size = [64, 150, 224]
+image_sizes = [64, 150, 224]
 thresholds = [0.5, 0.6, 0.7, 0.8, 0.9]
 l2s = [0.1, 0.01, 0.001]
-lrs = [0.01, 0.005, 0.001, 0.0005, 0.0001, 0.00005, 0.00001]
+lrs = [0.1, 0.05, 0.01, 0.005, 0.001, 0.0005, 0.0001, 0.00005, 0.00001]
 dropouts = [0.5, 0.75, 1.0]
-batch_sizes = [16, 32, 64, 128]
+batch_sizes = [16, 32, 64, 100, 128]
+# for image_size in image_sizes:
+#     for threshold in thresholds:
+#         pass
 # for l2 in l2s:
 #     config.l2 = l2
 #     for dropout in dropouts:
@@ -84,16 +94,35 @@ batch_sizes = [16, 32, 64, 128]
 #         for batch_size in batch_sizes:
 #             config.batch_size = batch_size
 #             total_batch_size = int(config.train_size / config.batch_size)
-#             for lr in lrs:
-#                 config.lr = lr
-#                 model.restore()
-#                 print('L2: %f, Dropout: %f, Batch Size: %d, Learning Rate: %f \n' %
-#                       (config.l2, config.dropout, config.batch_size, config.lr))
-#                 train(train_data, total_batch_size,
-#                       random.sample(validation_data, 100))
+for lr in lrs:
+    config.lr = lr
+    model.restore()
+    # print('L2: %f, Dropout: %f, Batch Size: %d, Learning Rate: %f \n' %
+    #       (config.l2, config.dropout, config.batch_size, config.lr))
+    print('Learning Rate: %f' % (lr))
+    total_batch_size = int(config.train_size / config.batch_size)
+    train(random.sample(train_data, model.config.train_size), total_batch_size,
+          random.sample(validation_data, model.config.valid_size))
 
-total_batch_size = int(config.train_size / config.batch_size)
+# for batch_size in batch_sizes:
+#     config.batch_size = batch_size
+#     model.restore()
+#     print('Batch Size: %f' % (batch_size))
+#     total_batch_size = int(config.train_size / config.batch_size)
+#     train(random.sample(train_data, model.config.train_size), total_batch_size,
+#           random.sample(validation_data, model.config.valid_size))
+
+# for threshold in thresholds:
+#     config.threshold = threshold
+#     model.restore()
+#     print('Threshold: %f' % (threshold))
+#     total_batch_size = int(config.train_size / config.batch_size)
+#     train(random.sample(train_data, model.config.train_size), total_batch_size,
+#           random.sample(validation_data, model.config.valid_size))
+
+
+# total_batch_size = int(config.train_size / config.batch_size)
 # val_batch_size = int(config.valid_size / config.batch_size)
-model.restore()
-train(random.sample(train_data, model.config.train_size), total_batch_size,
-      random.sample(validation_data, model.config.valid_size))
+# model.restore()
+# train(random.sample(train_data, model.config.train_size), total_batch_size,
+#       random.sample(validation_data, model.config.valid_size))
